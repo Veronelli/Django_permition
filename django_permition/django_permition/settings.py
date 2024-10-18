6b1b14c73ad82291ca1e72c6302482768a7fc29c
facu <facu@facus-Air.fibertel.com.ar>
2024-10-17 23:08:49 -0300

    add new envs

--- django_permition/django_permition/settings.py
+++ django_permition/django_permition/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,10 @@ WSGI_APPLICATION = 'django_permition.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'permition',
-        'USER': 'postgres',   
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'NAME': os.getenv('DJANGO_DATABASE_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': '5432',
     }
 }
